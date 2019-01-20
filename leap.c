e63100e63b4ce2fcb5937c335fcabebbea350cb0
dhina016 <46128375+dhina016@users.noreply.github.com>
2019-01-20 05:22:03 -0800

    Guvi

--- leap.c
+++ leap.c
@@ -1,19 +1,16 @@
-#include<stdio.h>
-void main()
+#include <stdio.h>
+
+void main() 
 {
-	int a,b,i,test;
-	scanf("%d",&test);
-	while(t)
-	{
-		scanf("%d%d",&a,&b);
-		for(i=a;i<=b;i++)
-		{
-			if((i%4==0)&&(i%100!=0)||(i%400==0))
-			{
-				printf("%d\t",i);	
-			}
-		}
-			t--;
-	}
-	getch();
+   int a;
+    scanf("%d",&a);
+   if(((a % 4 == 0) && (a % 100!= 0)) || (a %400 == 0))
+   {
+      printf("yes");
+   }
+   else
+   {
+      printf("no");
+   }
+   getch();
 }
